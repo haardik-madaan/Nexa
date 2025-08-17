2c2f70d0fc32f3afe92e08de36e815d485b91254
Haardik Madaan <madaanhaardik@gmail.com>
2025-08-17 17:02:59 +0530

    project setup

---
+++ app/provider.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+const Provider = ({children}) => {
+  return (
+    <div>
+        <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange>
+
+        {children}
+
+        </NextThemesProvider>
+        </div>
+  )
+}
+
+export default Provider
